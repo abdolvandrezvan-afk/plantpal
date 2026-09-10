56bf5e08984ad5fe98efbd8fe0b21b746e0a024a
Reyhane Gorji <abdolvandrezvan@gmail.com>
2026-09-10 04:53:37 -0700

    Initial commit: setup PlantPal project

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// PlantPal - فایل اصلی برنامه
+
+console.log('🌱 PlantPal در حال بارگذاری...');
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('✓ PlantPal با موفقیت بارگذاری شد');
+});
